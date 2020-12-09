84cdde7fe284aa585a9e0dec8b2c2caee70a7572
Adam AQUESBI <adam.aquesbi@epita.fr>
2020-12-09 21:39:10 +0100

    Minor changes

--- router/routes/root_route.py
+++ router/routes/root_route.py
@@ -10,9 +10,20 @@ def root_get():
 	store_id = request.args.get('store_id', default=0)
 	product_id = request.args.get('product_id', default=0)
 
+	str_response = "Undefined response"
+	status = 300
+
+	if store_id == 0 or product_id == 0:
+		str_response = ["Missing one or more parameters"]
+		status = 300
+	else:
+		str_response = [{'Response': ['Store id : {}'.format(store_id), 'Product id : {}'.format(product_id)] } ]
+		status = 200
+
+
 	response = app.response_class(
-		response = json.dumps("Returned string"),
-		status = 200, # OK
+		response = json.dumps(str_response),
+		status = status, # OK
 		mimetype='application/json'
 	)
 
