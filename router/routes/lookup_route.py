1e1768a3d309d55b0b6ecfa340c33a11bb32e231
Adam AQUESBI <adam.aquesbi@epita.fr>
2020-12-17 12:36:39 +0100

    Added a 80% lower limit to similarity

--- router/routes/lookup_route.py
+++ router/routes/lookup_route.py
@@ -3,7 +3,7 @@
 """
 
 from __main__ import app, request, json
-from __main__ import URI_PREFIX
+from __main__ import URI_PREFIX, MIN_SIMILARITY
 
 from scribe.postgresql.models.product_feature import ProductFeature
 from utils.comparator.cosine_distance import compare
@@ -30,15 +30,20 @@ def lookup_get():
 		
 		if product_features:
 			
-			str_response = [{'Request': ['Store id : {}'.format(store_id), 'Product id : {}'.format(product_id)]}, {'Response' : [product_features.picture_url, len(product_features.feature_data)]} ]
+			str_response = []
+			str_response.append({'Request': {'Store id': store_id, 'Product id': product_id } })
+			str_response.append({'Response' : {'picture url':product_features.picture_url, 'data size': len(product_features.feature_data), 'minimum similarity required': MIN_SIMILARITY } })
 			status = 200
 
 			comparison_results = []
 
 			for p in all_products_features:
-				comparison_results.append(compare(p.feature_data,product_features.feature_data))
+				similarity = compare(p.feature_data,product_features.feature_data)
 
-			print(comparison_results)
+				if similarity >= MIN_SIMILARITY:
+					comparison_results.append({'product id': p.product_id, 'store_id': p.store_id, 'similarity':similarity })
+
+			str_response.append({'similar products' :comparison_results})
 
 		else:
 			pass
